12dc10ba04bb51a2f931e479cd69f935d4614a1a
Akhror <akhsoliev@gmail.com>
2021-01-09 09:56:35 +0500

    changed style and fixed some bugs

--- src/components/orders/Order_item.js
+++ src/components/orders/Order_item.js
@@ -1,29 +1,27 @@
 import React, { useEffect } from 'react'
-import Card from '@material-ui/core/Card';
-import {useParams} from 'react-router-dom'
+import Card from '@material-ui/core/Card'
+import { useParams } from 'react-router-dom'
 import Map_side from './Map_side'
 import Left_side from './Left_side'
-import {getKur, getTaom} from '../../actions/userActions'
-import {connect} from 'react-redux'
-const Order_item = ({getKur, getTaom}) => {
-    const {id} = useParams()
-    useEffect(()=>{
-
-        getTaom(id)
-        const inter = setInterval(() => {
-            getKur()
-        }, 1000);
-        return () => {
-            clearInterval(inter)
-        }
-    
-    },[])
-    return (
-        <Card style={{padding: '0 15px', display:"flex"}}>
-           <Left_side id={id}></Left_side>
-           <Map_side id={id}></Map_side>
-        </Card>
-    )
+import { getKur, getTaom } from '../../actions/userActions'
+import { connect } from 'react-redux'
+const Order_item = ({ getKur, getTaom }) => {
+  const { id } = useParams()
+  useEffect(() => {
+    getTaom(id)
+    const inter = setInterval(() => {
+      getKur()
+    }, 1000)
+    return () => {
+      clearInterval(inter)
+    }
+  }, [])
+  return (
+    <Card style={{ display: 'flex' }}>
+      <Left_side id={id}></Left_side>
+      <Map_side id={id}></Map_side>
+    </Card>
+  )
 }
 
-export default connect(null, {getKur, getTaom})(Order_item)
+export default connect(null, { getKur, getTaom })(Order_item)
